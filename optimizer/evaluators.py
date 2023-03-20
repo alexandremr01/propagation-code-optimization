46a35b2e5ad629f5851b477a483f5f0a6c91803d
alexandre <alexandremr01@gmail.com>
2023-03-20 17:32:19 +0100

    add energy test

--- optimizer/evaluators.py
+++ optimizer/evaluators.py
@@ -90,18 +90,8 @@ class NaiveEvaluator(BaseEvaluator):
 
 #To use this class you must be root
 class EnergyEvaluator(BaseEvaluator): 
-    def __init__(self, olevel, simd, problem_size_x, problem_size_y, problem_size_z, nthreads, thrdblock_x, thrdblock_y, thrdblock_z) -> None:
-        self.olevel = olevel
-        self.simd = simd
-        self.problem_size_x = problem_size_x
-        self.problem_size_y = problem_size_y
-        self.problem_size_z = problem_size_z
-        self.nthreads = nthreads
-        self.thrdblock_x = thrdblock_x
-        self.thrdblock_y = thrdblock_y
-        self.thrdblock_z = thrdblock_z
-        self.calculated_cost = None
-        self.calculated_energy = None
+    def __init__(self, program_path, evaluation_session):
+        super().__init__(program_path, evaluation_session)
 
     def csv_to_energy(self,csv_path='temporary_csv_file.csv'):
         """takes a csv file from cpu_monitor and return the energy consumed"""
@@ -139,17 +129,17 @@ class EnergyEvaluator(BaseEvaluator):
 
         return dram_energy,pkg_energy,dram_energy+pkg_energy
     
-    def cost(self, evaluation_session, verbose=False, delete_file=True, num_evaluations=1):
-        if self.calculated_cost is not None:
-            return self.calculated_cost
-
-        evaluation_session.run_increase(num_evaluations)  # Increases in num_evaluations the counter of runs
+    def cost(self, solution, verbose=False, delete_file=True, num_evaluations=1, ignore_cache=False, affinity='balanced'):
+        program_path = self.program_path
+        if not ignore_cache and solution.calculated_cost is not None:
+            return solution.calculated_cost
+        self.evaluation_session.run_increase(num_evaluations)  # Increases in num_evaluations the counter of runs
 
         file_name = str(threading.get_ident())
         file_name_with_ext = f'{file_name}.exe'
-        executable_path = f'iso3dfd-st7/bin/{file_name_with_ext}'
+        executable_path = f'{program_path}/bin/{file_name_with_ext}'
 
-        result = subprocess.run(['make', '-C', 'iso3dfd-st7', f'Olevel={self.olevel}', f'simd={self.simd}', 'last'],
+        result = subprocess.run(['make', '-C', program_path, f'Olevel={self.olevel}', f'simd={self.simd}', 'last'],
                                 stdout=subprocess.DEVNULL,
                                 env=dict(os.environ, CONFIG_EXE_NAME=file_name_with_ext))
         if result.returncode != 0:
@@ -161,24 +151,24 @@ class EnergyEvaluator(BaseEvaluator):
         csv_path = 'temporary_csv_file.csv'
         for _ in range(num_evaluations):
             result_nrj = subprocess.run(['cpu_monitor_binary/releases/default/cpu_monitor.x','--csv',f'--csv-file={csv_path}','--quiet','--redirect',executable_path,
-                                     str(self.problem_size_x),
-                                     str(self.problem_size_y),
-                                     str(self.problem_size_z),
-                                     str(self.nthreads), '100',
-                                     str(self.thrdblock_x),
-                                     str(self.thrdblock_y),
-                                     str(self.thrdblock_z)], 
+                                     str(solution.problem_size_x),
+                                     str(solution.problem_size_y),
+                                     str(solution.problem_size_z),
+                                     str(solution.nthreads), '100',
+                                     str(solution.thrdblock_x),
+                                     str(solution.thrdblock_y),
+                                     str(solution.thrdblock_z)],
                                      capture_output=True,
                                      env=new_environment)
             energy = self.csv_to_energy('temporary_csv_file.csv')[2]
             result = subprocess.run([executable_path,
-                                     str(self.problem_size_x),
-                                     str(self.problem_size_y),
-                                     str(self.problem_size_z),
-                                     str(self.nthreads), '100',
-                                     str(self.thrdblock_x),
-                                     str(self.thrdblock_y),
-                                     str(self.thrdblock_z)], 
+                                     str(solution.problem_size_x),
+                                     str(solution.problem_size_y),
+                                     str(solution.problem_size_z),
+                                     str(solution.nthreads), '100',
+                                     str(solution.thrdblock_x),
+                                     str(solution.thrdblock_y),
+                                     str(solution.thrdblock_z)],
                                      capture_output=True,
                                      env=new_environment)
             if result.returncode != 0:
@@ -202,6 +192,5 @@ class EnergyEvaluator(BaseEvaluator):
 
         mean_throughput = round(mean_throughput/num_evaluations, 2)
         mean_energy = round(mean_energy/num_evaluations, 2)
-        self.calculated_cost = mean_throughput
-        self.calculated_energy = mean_energy
-        return mean_throughput/mean_energy
+        solution.calculated_cost = mean_throughput / mean_energy
+        return mean_throughput / mean_energy
